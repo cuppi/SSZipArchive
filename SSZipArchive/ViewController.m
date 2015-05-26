6413a818009da6f3832349a0c4b38cfb0bace0b6
ZhangTF <cuppi413@163.com>
2015-05-26 15:47:45 +0800

    finish

--- SSZipArchive/ViewController.m
+++ SSZipArchive/ViewController.m
@@ -8,15 +8,38 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <SSZipArchiveDelegate>
+{
+    UIImageView *_imageView;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _imageView = [[UIImageView alloc]init];
+    [self.view addSubview:_imageView];
+    //  _imageView.backgroundColor = [UIColor redColor];
+    _imageView.frame = CGRectMake(0, 0, 320, 568);
+    _imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    NSString *sourcePath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"zipManagerTest.zip"];
+    NSString *targetPath = [[(NSArray *)NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)firstObject] stringByAppendingPathComponent:@"zipManagerTestRelease"];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if([[NSFileManager defaultManager] fileExistsAtPath:sourcePath])
+    {
+        [SSZipArchive unzipFileAtPath:sourcePath toDestination:targetPath delegate:self];
+    }
+    else
+    {
+    }
+    
+    
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +47,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)zipArchiveDidUnzipArchiveAtPath:(NSString *)path zipInfo:(unz_global_info)zipInfo unzippedPath:(NSString *)unzippedPath
+{
+    NSLog(@"path%@",path);
+    NSLog(@"unzippedPath %@", unzippedPath);
+    NSString *imageString = [[unzippedPath  stringByAppendingPathComponent:@"zipManagerTest"]stringByAppendingPathComponent:@"testImage.jpg"];
+    
+    if([[NSFileManager defaultManager] fileExistsAtPath:imageString])
+    {
+        NSLog(@"图片存在");
+        _imageView.image = [UIImage imageWithContentsOfFile:imageString];
+        NSLog(@"%@",_imageView.image);
+    }
+    NSLog(@"size_comment %lu",zipInfo.size_comment);
+    NSLog(@"number_entry %lu", zipInfo.number_entry);
+    
+}
 @end
